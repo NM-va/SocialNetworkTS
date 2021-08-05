9c62a749b2feee4b05101f2ca138957a155816ca
NM-va <nad.n.mwa@gmail.com>
2021-08-06 01:54:24 +0300

    delete unused

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,21 +1,10 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './Dialogs.module.css';
+import React from "react";
+import styles from "./Dialogs.module.css";
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {AddMessages} from "./AddMessages/AddMessages";
 import {ActionTypes, DialogType, MessageType} from "../../redux/state";
 
-//
-// type DialogType = {
-//     id: number
-//     name: string
-// }
-//
-// type MessageType = {
-//     id: number
-//     message: string
-// }
 
 type PropsType = {
     dialogs: Array<DialogType>
