930acfadfbecaa81a6e97302729f6774683f3299
NM-va <nad.n.mwa@gmail.com>
2021-11-07 21:33:33 +0300

    didUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -29,6 +29,14 @@ export class ProfileStatus extends React.Component<PropsType> {
         this.setState({status: e.currentTarget.value})
     }
     
+    componentDidUpdate(prevProps: PropsType, prevState: PropsType, snapshot?: any) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+    
     render() {
         
         return (
