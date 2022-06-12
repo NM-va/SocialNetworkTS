54b24f01ae3a5cefe49f1630f6cd4c29ca1671cb
NM-va <nad.n.mwa@gmail.com>
2022-06-12 22:38:39 +0300

    stylization pagination

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import styles from "./Pagination.module.scss";
 import classNames from 'classnames/bind';
+
 let cn = classNames.bind(styles);
 
 
@@ -31,32 +32,34 @@ export const Pagination = ({
     let [portionNumber, setPortionNumber] = useState(1);
     let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
     let rightPortionPageNumber = portionNumber * portionSize;
-    
-    return (
-        <ul className={styles.pagination}>
-            {portionNumber > 1 &&
-                <li>
-                    <button onClick={() => setPortionNumber(portionNumber - 1)}>Prev</button>
-                </li>
-            }
 
-            {
-                pages.map(page => {
-                    return (
-                        <li className={cn({[styles.selectedPage]: currentPage === page}, styles.pageItem)}
-                            key={page}
-                            onClick={(e) => {
-                                onPageChanged(page)
-                            }}
-                        >{page}</li>
-                    )
-                })
-            }
-            {portionCount > portionNumber &&
-                <li>
-                    <button onClick={() => setPortionNumber(portionNumber + 1)}>Next</button>
-                </li>
-            }
-        </ul>
+    return (
+        <nav>
+            <ul className={"pagination"}>
+                {portionNumber > 1 &&
+                    <li className={"page-item"}>
+                        <span className={"page-link"} onClick={() => setPortionNumber(portionNumber - 1)}>Prev</span>
+                    </li>
+                }
+                {
+                    pages
+                        .filter(((page: number) => page >= leftPortionPageNumber && page <= rightPortionPageNumber ))
+                        .map(page => {
+                        return (
+                            <li className={` ${cn({active: currentPage === page}, "page-item")}`} key={page}>
+                                <span className={"page-link"}  onClick={(e) => {
+                                    onPageChanged(page)
+                                }}>{page}</span>
+                            </li>
+                        )
+                    })
+                }
+                {portionCount > portionNumber &&
+                    <li className={"page-item"}>
+                        <span className={"page-link"} onClick={() => setPortionNumber(portionNumber + 1)}>Next</span>
+                    </li>
+                }
+            </ul>
+        </nav>
     )
-}
+};
