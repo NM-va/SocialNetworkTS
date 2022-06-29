24093bac52fce8bcf1753c9be579c72acdf55f47
NM-va <nad.n.mwa@gmail.com>
2022-06-30 02:24:55 +0300

    page settings stylization

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styles from "./Settings.module.scss";
+
 
 export const Settings = () => {
     return (
-        <div>
-            Settings
+        <div className="card cardItem">
+            <div className={styles.greeting}>
+                Thank's for coming!
+                <i className="bi bi-emoji-smile"></i>
+            </div>
         </div>
     )
 }
