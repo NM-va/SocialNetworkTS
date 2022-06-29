b894d32c317fa6f996a8f7bbc2e1a5aea015809b
NM-va <nad.n.mwa@gmail.com>
2022-06-30 02:23:09 +0300

    music replace songs

--- src/redux/music-reducer.ts
+++ src/redux/music-reducer.ts
@@ -10,73 +10,73 @@ let initialState = {
     songList: [
         {
             id: 1,
-            name: 'Sanctified with Dynamite',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://cdnb.artstation.com/p/assets/images/images/010/532/065/large/zsofia-dankova-1.jpg?1539776234'
+            name: 'This is Japan',
+            author: 'FREE FLOW FLAVA',
+            duration: '2:42',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/5966316/476b306e.a.22439911-1/200x200'
         },
         {
             id: 2,
-            name: 'Army of the Night',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://i.pinimg.com/originals/10/37/36/1037361b721513a7168e1dae07139f55.jpg'
+            name: 'Death Ninja',
+            author: 'FREE FLOW FLAVA',
+            duration: '3:38',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/4382102/bfb762cc.a.17672843-1/200x200'
         },
         {
             id: 3,
-            name: 'Higher Than Heaven',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b81633f7-ac45-4e1d-9255-46297d588240/dcf39re-204aaff0-a53f-4f9b-83d3-81239bf35778.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I4MTYzM2Y3LWFjNDUtNGUxZC05MjU1LTQ2Mjk3ZDU4ODI0MFwvZGNmMzlyZS0yMDRhYWZmMC1hNTNmLTRmOWItODNkMy04MTIzOWJmMzU3NzgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.fE1cp7I7GjauqJ8gKCoqz2cK1BHjeAwhivRnE7oMsVo'
+            name: 'Red Dragon',
+            author: 'FREE FLOW FLAVA',
+            duration: '1:41',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/4388221/16a6b4d0.a.17672880-1/200x200'
         },
         {
             id: 4,
-            name: 'Incense & Iron',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://i.pinimg.com/originals/2b/ca/63/2bca632180d842a6f15908154ce862bb.jpg'
+            name: 'Mistake',
+            author: 'FREE FLOW FLAVA',
+            duration: '2:45',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/1880735/70b6d66c.a.9255001-1/200x200'
         },
         {
             id: 5,
-            name: 'Venom of Venus',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://steamuserimages-a.akamaihd.net/ugc/941709259346307842/830C554F58DDEF61ACD21D28FBC3FC4FEAAAE136/'
+            name: 'Ray of Hope',
+            author: 'FREE FLOW FLAVA',
+            duration: '1:59',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/5234847/15fd0031.a.17672925-1/200x200'
         },
         {
             id: 6,
-            name: 'Sanctified with Dynamite',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://cdnb.artstation.com/p/assets/images/images/010/532/065/large/zsofia-dankova-1.jpg?1539776234'
+            name: '014',
+            author: 'FREE FLOW FLAVA',
+            duration: '2:02',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/2383988/9ec0cdf1.a.10166367-1/200x200'
         },
         {
             id: 7,
-            name: 'Army of the Night',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://i.pinimg.com/originals/10/37/36/1037361b721513a7168e1dae07139f55.jpg'
+            name: '002',
+            author: 'FREE FLOW FLAVA',
+            duration: '2:01',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/2383988/9ec0cdf1.a.10166367-1/200x200'
         },
         {
             id: 8,
-            name: 'Higher Than Heaven',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b81633f7-ac45-4e1d-9255-46297d588240/dcf39re-204aaff0-a53f-4f9b-83d3-81239bf35778.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I4MTYzM2Y3LWFjNDUtNGUxZC05MjU1LTQ2Mjk3ZDU4ODI0MFwvZGNmMzlyZS0yMDRhYWZmMC1hNTNmLTRmOWItODNkMy04MTIzOWJmMzU3NzgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.fE1cp7I7GjauqJ8gKCoqz2cK1BHjeAwhivRnE7oMsVo'
+            name: 'Eternal',
+            author: 'FREE FLOW FLAVA',
+            duration: '2:09',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/5280749/8c10dbcd.a.17678543-1/200x200'
         },
         {
             id: 9,
-            name: 'Incense & Iron',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://i.pinimg.com/originals/2b/ca/63/2bca632180d842a6f15908154ce862bb.jpg'
+            name: 'Itachi Uchiha',
+            author: 'FREE FLOW FLAVA',
+            duration: '1:57',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/5280749/c9a7d301.a.17672907-1/200x200'
         },
         {
             id: 10,
-            name: 'Venom of Venus',
-            author: 'PowerWolf',
-            duration: '3:51',
-            imgSrc: 'https://steamuserimages-a.akamaihd.net/ugc/941709259346307842/830C554F58DDEF61ACD21D28FBC3FC4FEAAAE136/'
+            name: '001',
+            author: 'FREE FLOW FLAVA',
+            duration: '2:05',
+            imgSrc: 'https://avatars.yandex.net/get-music-content/2383988/9ec0cdf1.a.10166367-1/200x200'
         },
     ] as Array<SongItemType>
 };
