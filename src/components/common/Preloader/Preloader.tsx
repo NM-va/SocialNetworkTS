314eb9ed6cc2039f42460390572afdc274d53831
NM-va <nad.n.mwa@gmail.com>
2022-06-30 14:05:55 +0300

    preloader stylization

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import preloaderImg from '../../../assets/images/preloader.gif';
+import React from "react";
+import preloaderImg from "../../../assets/images/preloader.gif";
+import styles from "./Preloader.module.scss";
 
 export const Preloader = () => {
     return  (
-        <div>
+        <div className={styles.preloader}>
             <img src={preloaderImg} />
         </div>
     )
-}
+};
